bfd24eee24dcdf87342147e87a39632dd0b44b26
Tommy The Turtle <thsetrof@tuta.io>
2024-03-28 11:16:56 +0000

    add function to create a new book with the user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookContainer = document.querySelector('.container')
 
 function Book(title, auhtor, pages, read = false){
     this.title = title
@@ -7,6 +8,22 @@ function Book(title, auhtor, pages, read = false){
     this.read = read
 }
 
+myLibrary.push(new Book('Midnight', 'Kelvin Monteiro', 112))
+myLibrary.push(new Book('O Homem do Futuro', 'Thomas Setrof', 256))
+myLibrary.push(new Book('To the moon', 'Hiroshi Nakata', 354))
+
+
 function addBookToLibrary(){
-    
+    let title = prompt('Title: ');
+    let author = prompt('Author: ');
+    let pages = prompt('Nr. Pages: ');
+
+    const book = new Book(title, author, pages);
+
+    myLibrary.push(book);
+
+}
+
+function displayBooks(){
+
 }
