6a7735138e524475f0e8b248e00fd5dddb867b7f
Tommy The Turtle <thsetrof@tuta.io>
2024-03-28 11:05:41 +0000

    add html skeleton and script with Book object

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, auhtor, pages, read = false){
+    this.title = title
+    this.auhtor = auhtor
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(){
+    
+}
